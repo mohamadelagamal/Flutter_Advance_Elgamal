f70992ff7583c2e93062de6160dc56d04bae589a
mohamad Elgamal <mohamadelgamal2002.9.1@gmail.com>
2024-09-21 20:57:40 +0300

    added icon for app added launch json for flavor config. added Development & Production flavor for android changed andorid app name for eatch flavor added to main for eatch flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advance_elgamal/doc_app.dart';
-
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
   setUpGetIt();
-  runApp( DocApp(
-    appRouter: AppRouter(),
-  ));
+  runApp(DocApp(appRouter: AppRouter()));
 }
-
-
